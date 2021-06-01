0fb340a14854f6e0ede9ba48c00e2cab060ba91f
qixiaoling <x.qi@novi-education.nl>
2021-06-01 10:01:53 +0200

    correction

--- src/Exercise/Todo/TodoApp.js
+++ src/Exercise/Todo/TodoApp.js
@@ -2,30 +2,31 @@ import React, {useState} from 'react'
 import todosData from "../../todosData";
 import TodoItems from "./TodoItems";
 
-function TodoApp(){
+function TodoApp() {
     const [items, setItems] = useState(todosData);
-    function handleClick(id){
-        const updatedTodo = items.map((item)=>{
-            if(item.id === id){
+
+    function handleClick(id) {
+        const updatedTodo = items.map((item) => {
+            if (item.id === id) {
                 item.completed = !item.completed
             }
             return item;
         })
         setItems(updatedTodo)
     }
-    return(
+
+    return (
         <div>
-            {items.map((item)=>{
-                return(
+            {items.map((item) => {
+                return (
                     <TodoItems key={item.id} {...item}
-                    handleClick = {handleClick}/>
+                               handleClick={handleClick}/>
                 )
             })}
         </div>
     )
 
 
-
-
 }
+
 export default TodoApp
